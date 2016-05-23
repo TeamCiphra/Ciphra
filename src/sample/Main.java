fefcd0c782ae8b049332c1323fc71fadb7dbc16a
Ramen (Adam) <adamsalem99@gmail.com>
2016-05-23 11:51:09 -0700

    First Commit
    get ur ass in gear chris

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,5 @@
 package sample;
-
-//Import
+import java.util.Random;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.HPos;
@@ -19,189 +18,84 @@ import javafx.scene.control.Label;
 
 public class Main extends Application{
 
-    //Scene Variables
     Stage window;
-    Scene mainScene, items;
-    final Insets GLOBAL_INSET = new Insets(5, 5, 5, 5);
-
+    Scene attackScene, items;
     @Override
-    public void start(Stage mainStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception{
+        Random rand = new Random();
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        window = primaryStage;
+        Label attackLabel = new Label("This is the attack scene");
+        primaryStage.setTitle("Attack Scene Test");
 
-        //Main Stage
-        window = mainStage;
+        Image imageItem = new Image(getClass().getResourceAsStream("item.png"));
+        Button item = new Button();
+        item.setGraphic(new ImageView(imageItem));
+        item.setStyle("-fx-base: #8080ff; -fx-focus-color: transparent");
+        item.setOnAction(e -> window.setScene(items));
 
-        //Title
-        mainStage.setTitle("Game Desu.exe");
+        Image imageAttack = new Image(getClass().getResourceAsStream("attackMelee.png"));
+        Button attack = new Button();
+        attack.setGraphic(new ImageView(imageAttack));
+        attack.setStyle("-fx-base: #8080ff; -fx-focus-color: transparent");
+        Image imageBigDamage = new Image(getClass().getResourceAsStream("attackMagic.png"));
+        attack.setOnAction(e -> Alerts.display("Warning!", imageBigDamage, (rand.nextInt(80) + 21) + " DAMAGE!!!"));
 
-        //GridPanes
-        GridPane sceneBattle = new GridPane();//Battle Scene GridPane
-        GridPane coreGridPane = new GridPane();//Core GridPane
-        GridPane attackGrid = new GridPane();//Attack GridPane
-        GridPane buttonGrid = new GridPane();//Button Field GridPane
+        Image imageRun = new Image(getClass().getResourceAsStream("run.png"));
+        Button run = new Button();
+        run.setGraphic(new ImageView(imageRun));
+        run.setStyle("-fx-base: #8080ff; -fx-focus-color: transparent");
+        Image imageRunAway = new Image(getClass().getResourceAsStream("run.png"));
+        run.setOnAction(e -> Alerts.display("Warning!", imageRunAway, "RUN AWAY"));
 
-        //Image Loading Start
+        Image imageHealth = new Image(getClass().getResourceAsStream("greenBar.png"));
+        Label health = new Label();
+        health.setGraphic(new ImageView(imageHealth));
+        health.setStyle("-fx-base: #8080ff; -fx-focus-color: transparent");
+        Image imageHealthBar = new Image(getClass().getResourceAsStream("greenBar.png"));
+        health.(e -> Alerts.display("Your Health", imageHealthBar, (rand.nextInt(40)+1) + "/40"));
 
-        //Attack
-//        ImageView imageAttackMelee = new ImageView(new Image(getClass().getResourceAsStream("attackMelee.png")));
-//        imageAttackMelee.setFitWidth(64);
-//        imageAttackMelee.setFitHeight(64);
-        ImageView imageBigDamage = new ImageView(new Image(getClass().getResourceAsStream("bigdamage.jpg")));
-        ImageView imageAttackMagic = new ImageView(new Image(getClass().getResourceAsStream("attackMagic.png")));
-        imageAttackMagic.setFitWidth(64);
-        imageAttackMagic.setFitHeight(64);
+        Image enemyHealth = new Image(getClass().getResourceAsStream("greenBar.png"));
+        Button eHealth = new Button();
+        eHealth.setGraphic(new ImageView(enemyHealth));
+        eHealth.setStyle("-fx-base: #8080ff; -fx-focus-color: transparent");
+        Image imageEHealthBar = new Image(getClass().getResourceAsStream("greenBar.png"));
+        eHealth.setOnAction(e -> Alerts.display("Enemy Health", imageEHealthBar, (rand.nextInt(40)+1) + "/40"));
 
-        //Item
-        ImageView imageItem = new ImageView(new Image(getClass().getResourceAsStream("item.png")));
-        imageItem.setFitWidth(64);
-        imageItem.setFitHeight(64);
+        GridPane battle = new GridPane();
+        battle.setGridLinesVisible(false);
+        battle.setPrefSize(854, 480);
+        battle.setVgap(10);
+        battle.setHgap(10);
+        battle.setPadding(new Insets(10, 10, 10, 10));
+        battle.setAlignment(Pos.CENTER);
 
-        //Run
-        ImageView imageRun = new ImageView(new Image(getClass().getResourceAsStream("run.png")));
-        imageRun.setFitWidth(64);
-        imageRun.setFitHeight(64);
-        ImageView imageRunAway = new ImageView(new Image(getClass().getResourceAsStream("runaway.jpg")));
+        GridPane.setConstraints(attackLabel, 0, 2);
+        GridPane.setConstraints(attack, 1, 3);
+        GridPane.setConstraints(item, 2, 3);
+        GridPane.setConstraints(run, 3, 3);
+        GridPane.setConstraints(health, 0, 0);
+        GridPane.setConstraints(eHealth, 3, 0);
 
-        //Player
-        ImageView playerOne = new ImageView(new Image(getClass().getResourceAsStream("playerone.jpg")));
-        ImageView playerTwo = new ImageView(new Image(getClass().getResourceAsStream("playertwo.jpg")));
+        battle.getChildren().addAll(attackLabel, attack, item, run, health, eHealth);
+        battle.setHalignment(attackLabel, HPos.CENTER);
+        battle.setHalignment(attack, HPos.CENTER);
+        battle.setHalignment(item, HPos.CENTER);
+        battle.setHalignment(run, HPos.CENTER);
+        battle.setHalignment(health, HPos.CENTER);
+        battle.setHalignment(eHealth, HPos.CENTER);
 
-        //Image Loading End
+        attackScene = new Scene(battle, 854, 480);
 
-        //Labels
-        Label playerOneSprite = new Label();
-        playerOneSprite.setGraphic(playerOne);
-        Label playerOneWeapon = new Label("This is player One's weapon");
-        Label playerTwoSprite = new Label();
-        playerTwoSprite.setGraphic(playerTwo);
-        Label playerTwoWeapon = new Label("This is player Two's weapon");
-        Label battlePrompt = new Label("Please choose an option.");
-
-        //Buttons
-        Button attack = new Button();//Attack Button
-        Button attackMelee = new Button();//AttackMelee Button
-        Button attackMagic = new Button();//AttackMagic Button
-        Button item = new Button();//Item Button
-        Button run = new Button();//Run Button
-
-        //Button Configs Start
-
-        //Attack Button
-        attack.setGraphic(ImageLoader.ImageLoader("attack"));//Set Button Image
-        attack.setStyle("-fx-base: #8080ff; -fx-focus-color: transparent");//Button Formatting
-        attack.setOnAction(e -> {
-                    coreGridPane.getChildren().remove(battlePrompt);
-                    coreGridPane.add(attackGrid, 0, 1);
-                    coreGridPane.getChildren().remove(buttonGrid);
-                    Button goBack = new Button("Go back");
-                    coreGridPane.add(goBack, 1, 1);
-                    goBack.setOnAction(f -> {
-                        coreGridPane.getChildren().remove(attackGrid);
-                        coreGridPane.add(battlePrompt, 0, 1);
-                        coreGridPane.getChildren().remove(goBack);
-                        coreGridPane.add(buttonGrid, 1, 1);
-                    }
-                    );
-        }
-        );//Lambda Action
-
-        //Attack Melee
-        attackMelee.setGraphic(ImageLoader.ImageLoader("attack"));
-        attackMelee.setStyle("-fx-base: #8080ff; -fx-focus-color: transparent");
-
-        //Attack Magic
-        attackMagic.setGraphic(imageAttackMagic);
-        attackMagic.setStyle("-fx-base: #8080ff; -fx-focus-color: transparent");
-
-        //Item Button
-        item.setGraphic(imageItem);//Set Button Image
-        item.setStyle("-fx-base: #8080ff; -fx-focus-color: transparent");//Button Formatting
-        item.setOnAction(e -> window.setScene(items));//Lambda Action
-
-        //Run Button
-        run.setGraphic(imageRun);//Set Button Image
-        run.setStyle("-fx-base: #8080ff; -fx-focus-color: transparent");//Button formatting
-        run.setOnAction(e -> Alerts.display("Warning!", imageRunAway, "RUN AWAY"));//Lambda Action
-
-        //Button Config End
-
-        //GridPane Config Start
-
-        //Main GP Formatting
-        coreGridPane.setVgap(10);
-        coreGridPane.setHgap(10);
-        coreGridPane.setPadding(GLOBAL_INSET);
-        coreGridPane.setAlignment(Pos.CENTER);
-        //Main GP GP Placement
-        coreGridPane.setConstraints(sceneBattle, 0, 0);
-        coreGridPane.setConstraints(battlePrompt, 0, 1);
-        coreGridPane.setConstraints(buttonGrid, 1, 1);
-
-        //Add Elements
-        coreGridPane.getChildren().addAll(sceneBattle, battlePrompt, buttonGrid);
-        coreGridPane.setHalignment(sceneBattle, HPos.CENTER);
-        coreGridPane.setHalignment(battlePrompt, HPos.CENTER);
-        coreGridPane.setHalignment(buttonGrid, HPos.CENTER);
-
-        //Battle Scene
-        sceneBattle.setHgap(10);
-        sceneBattle.setPadding(GLOBAL_INSET);
-        sceneBattle.setConstraints(playerOneSprite, 0, 0);
-        sceneBattle.setConstraints(playerOneWeapon, 1, 0);
-        sceneBattle.setConstraints(playerTwoWeapon, 2, 0);
-        sceneBattle.setConstraints(playerTwoSprite, 3, 0);
-        //Battle Scene Add Elements
-        sceneBattle.getChildren().addAll(playerOneSprite, playerOneWeapon, playerTwoWeapon, playerTwoSprite);
-        sceneBattle.setHalignment(playerOneSprite, HPos.CENTER);
-        sceneBattle.setHalignment(playerOneWeapon, HPos.CENTER);
-        sceneBattle.setHalignment(playerTwoWeapon, HPos.CENTER);
-        sceneBattle.setHalignment(playerTwoSprite, HPos.CENTER);
-
-
-        //Attack Action Grid
-        attackGrid.setGridLinesVisible(true);
-        attackGrid.setHgap(10);
-        attackGrid.setPadding(GLOBAL_INSET);
-        attackGrid.setAlignment(Pos.CENTER);
-        attackGrid.setConstraints(attackMelee, 0, 0);
-        attackGrid.setConstraints(attackMagic, 1, 0);
-        //Attack Action Grid Add Elements
-        attackGrid.getChildren().addAll(attackMelee, attackMagic);
-        attackGrid.setHalignment(attackMelee, HPos.CENTER);
-        attackGrid.setHalignment(attackMagic, HPos.CENTER);
-
-
-        //Main Button Grid
-        buttonGrid.setHgap(10);
-        buttonGrid.setPadding(GLOBAL_INSET);
-        buttonGrid.setAlignment(Pos.CENTER);
-        buttonGrid.setConstraints(attack, 0, 0);
-        buttonGrid.setConstraints(item, 1, 0);
-        buttonGrid.setConstraints(run, 2, 0);
-        //Main Button Grid Add Elements
-        buttonGrid.getChildren().addAll(attack, item, run);
-        buttonGrid.setHalignment(attack, HPos.CENTER);
-        buttonGrid.setHalignment(item, HPos.CENTER);
-        buttonGrid.setHalignment(run, HPos.CENTER);
-
-
-        //GridPane Config End
-
-
-        //Build Main
-        mainScene = new Scene(coreGridPane, 854, 480);
-
-        //New Back Button for Bag
         Button back = new Button("Go back");
-        back.setOnAction(e -> window.setScene(mainScene));//Lambda Action
+        back.setOnAction(e -> window.setScene(attackScene));
 
-        //SP for new image
         StackPane itemBag = new StackPane();
         itemBag.getChildren().add(back);
-        items = new Scene(itemBag,854, 480);
+        items = new Scene(itemBag,200,200);
 
-        window.setScene(mainScene);//Make the window Scene
-        mainStage.show();//Show
+        window.setScene(attackScene);
+        primaryStage.show();
     }
 
     public static void main(String[]args){
